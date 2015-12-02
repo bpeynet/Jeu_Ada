38e5e7c8d1f84c475d17ea0d68a2f037a50589e6
desmonsf <floriandesmons@gmail.com>
2015-12-02 14:33:13 +0100

    Est_gagnant sur le dernier coup seulment

--- puissance4.adb
+++ puissance4.adb
@@ -16,15 +16,19 @@ package body Puissance4 is
       return Nouvel_Etat;
    end Jouer;
 
+
    function Est_Gagnant(E : in Etat; Jo : in Joueur) return Boolean is
-      Valeur_Jo: Integer;
+      Valeur_Jo, I, J: Integer;
    begin
+	--On regarde quel à quel joueur appartient le pion
       if Jo=Joueur1 then Valeur_Jo:=1;
       elsif Jo=Joueur2 then Valeur_Jo:=2;
       else return False;
       end if;
-      for I in E.G'First(1)..E.G'Last(1) loop
-         for J in E.G'First(2)..E.G'Last(2) loop
+
+	--On appelle une fonction regardant 
+	I:=E.dernier_coup.Coordonnees(1);
+	J:=E.dernier_coup.Coordonnees(2);
             if E.G(I,J)=Valeur_Jo then
                if Alignement(E.G,I,J,I-1,J-1,1) or
                  Alignement(E.G,I,J,I-1,J,1) or
@@ -32,21 +36,25 @@ package body Puissance4 is
                  Alignement(E.G,I,J,I,J-1,1) or
                  Alignement(E.G,I,J,I,J+1,1) or
                  Alignement(E.G,I,J,I+1,J-1,1) or
-                 Alignement(E.G,I,J,I+1,J,1) or
+                 --Alignement(E.G,I,J,I+1,J,1) or
                  Alignement(E.G,I,J,I+1,J+1,1) then
                   return True;
                end if;
             end if;
-         end loop;
-      end loop;
+
       return False;
    end Est_Gagnant;
 
+
    function Alignement(G: access Grille; I: Integer; J: Integer; SI: Integer; SJ: Integer; Compteur: Integer) return Boolean is
+
    begin--Compteur doit partir de 1
+
       if I>=G'First(1) and I<=G'Last(1) and J>=G'First(2) and J<=G'Last(2) and
         SI>=G'First(1) and SI<=G'Last(1) and SJ>=G'First(2) and SJ<=G'Last(2) then
+
          if Compteur=Nb_alignes then return True;
+
          else
             if G(SI,SJ)=G(I,J) then 
                if Alignement(G,SI,SJ,2*SI-I,2*SJ-J,Compteur+1) or
@@ -57,8 +65,11 @@ package body Puissance4 is
             else return False;
             end if;
          end if;
+
       else return False;
+
       end if;
+
    end Alignement;
 
    -- Indique si l'etat courant est un status quo (match nul)
@@ -101,24 +112,8 @@ package body Puissance4 is
       Put_Line("Jouer dans la colonne" & Integer'Image(C.Coordonnees(2)));
    end Affiche_Coup;
 
-   --     -- Retourne le prochaine coup joue par le joueur1
-   --     function Demande_Coup_Joueur1(E : Etat) return Coup is
-   --        Ind_Colonne, I: Integer;
-   --        C: Coup;
-   --     begin
-   --        I:=E.G'Last(1)+1;
-   --        while I>E.G'Last(1) loop
-   --           Put_Line(", dans quelle colonne jouer ?");
-   --           Get(Ind_Colonne);
-   --           I:=E.G'First(1);
-   --           while Ind_Colonne<=E.G'Last(2) and Ind_Colonne>=E.G'First(2) and I<=E.G'Last(1) and E.G(I,Ind_Colonne)/=0 loop
-   --              I:=I+1;
-   --           end loop;
-   --        end loop;
-   --        C.Coordonnees:=(I,Ind_Colonne);
-   --        C.Qui:=Joueur1;
-   --        return C;
-   --     end Demande_Coup_Joueur1;
+
+
 
    function Demande_Coup_Joueur1(E : Etat) return Coup is
       Ind_Colonne, I: Integer;
@@ -156,25 +151,6 @@ package body Puissance4 is
 
    end Demande_Coup_Joueur1;
    
-   
-   --     -- Retourne le prochaine coup joue par le joueur2
-   --     function Demande_Coup_Joueur2(E : Etat) return Coup is
-   --        Ind_Colonne, I: Integer;
-   --        C: Coup;
-   --     begin
-   --        I:=E.G'Last(1)+1;
-   --        while I>E.G'Last(1) loop
-   --           Put_Line(", dans quelle colonne jouer ?");
-   --           Get(Ind_Colonne);
-   --           I:=E.G'First(1);
-   --           while Ind_Colonne<=E.G'Last(2) and Ind_Colonne>=E.G'First(2) and I<=E.G'Last(1) and E.G(I,Ind_Colonne)/=0 loop
-   --              I:=I+1;
-   --           end loop;
-   --        end loop;
-   --        C.Coordonnees:=(I,Ind_Colonne);
-   --        C.Qui:=Joueur2;
-   --        return C;
-   --     end Demande_Coup_Joueur2;
 
    -- Retourne le prochaine coup joue par le joueur2
    function Demande_Coup_Joueur2(E : Etat) return Coup is
